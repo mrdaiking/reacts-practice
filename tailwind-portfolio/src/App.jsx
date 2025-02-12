4a920643e659468004df906e143839746979d11e
Andy <andy@Ais-MacBook-Pro.local>
2025-02-13 07:57:52 +0900

    create first tailwind project v3.4

--- tailwind-portfolio/src/App.jsx
+++ tailwind-portfolio/src/App.jsx
@@ -1,18 +1,5 @@
-import About from './components/About'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Skills />
-      <About />
-      <Projects />
-    </>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
+
 export default App
